417bfc59a74fbc7df059a0c50acbcfff93e3ecfb
Vilius Paskonis <vilius.paskonis@gmail.com>
2024-12-27 21:56:50 +0200

    Implement winning and losing conditions. Implement menu bar with difficulty selection

--- main.cpp
+++ main.cpp
@@ -12,9 +12,11 @@ void PaintComponents(HWND, HINSTANCE);
 void PaintGrid(HDC);
 void DrawColorText(HDC, RECT, char*, COLORREF);
 void PaintValues(HDC, HINSTANCE);
+void PaintFailedMine(int, int, HWND, HINSTANCE);
 void InitializeGrid(int, int);
 void RevealCells(int, int);
 bool areNeighbors(int, int, int , int);
+void ResetGame(HWND, int, int);
 void PrintGrid();
 
 TCHAR szClassName[ ] = _T("Minesweeper");
@@ -26,10 +28,13 @@ struct Cell {
     int adjacentMines;
 };
 
-vector<vector<Cell> > grid(GRID_SIZE, vector<Cell>(GRID_SIZE));
-HWND gridButtons[GRID_SIZE][GRID_SIZE];
-HWND mineButton;
+int gridSize = GRID_SIZE;
+int numMines = NUM_MINES;
+int revealedCells = 0;
+vector<vector<Cell> > grid(gridSize, vector<Cell>(gridSize));
+vector<vector<HWND> > gridButtons(gridSize, vector<HWND>(gridSize));
 bool isGridInitialized = false;
+HMENU hMenu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MYMENU));;
 
 int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
 {
@@ -57,13 +62,13 @@ int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpsz
         0,
         szClassName,
         _T("Minesweeper"),
-        WS_OVERLAPPEDWINDOW,
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
-        GRID_SIZE * CELL_SIZE + 16 + BORDER_WIDTH * 2,
-        GRID_SIZE * CELL_SIZE + 36 + TOOLBAR_HEIGHT + BORDER_WIDTH,
+        gridSize * CELL_SIZE + 16 + BORDER_WIDTH * 2,
+        gridSize * CELL_SIZE + 36 + TOOLBAR_HEIGHT + BORDER_WIDTH + NAV_BAR_HEIGHT,
         HWND_DESKTOP,
-        NULL,
+        hMenu,
         hThisInstance,
         NULL);
 
@@ -80,7 +85,9 @@ int WINAPI WinMain (HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpsz
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    static HINSTANCE hInstance;
+    static HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
+    HICON hButtonIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ID_BUTTON_ICON));
+    HICON hFlagIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ID_FLAG_ICON));
 
     switch (message)
     {
@@ -90,13 +97,11 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
 
         case WM_CREATE:
         {
-            hInstance = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
-            HICON hButtonIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ID_BUTTON_ICON));
-            HICON hFlagIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ID_FLAG_ICON));
 
-            for (int x = 0; x < GRID_SIZE; ++x)
+            // Grid buttons
+            for (int x = 0; x < gridSize; ++x)
             {
-                for (int y = 0; y < GRID_SIZE; ++y)
+                for (int y = 0; y < gridSize; ++y)
                 {
                     gridButtons[x][y] = CreateWindow(
                         _T("BUTTON"),
@@ -107,44 +112,65 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
                         CELL_SIZE,
                         CELL_SIZE,
                         hwnd,
-                        (HMENU)(x * GRID_SIZE + y), // ButtonID
+                        (HMENU)(x * gridSize + y), // ButtonID
                         hInstance,
                         NULL);
 
                     SendMessage(gridButtons[x][y], BM_SETIMAGE, IMAGE_ICON, (LPARAM)hButtonIcon);
                 }
             }
-
-            mineButton = CreateWindow(
-                        _T("BUTTON"),
-                        _T(""),
-                        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON | BS_ICON,
-                        0,
-                        0,
-                        CELL_SIZE,
-                        CELL_SIZE,
-                        hwnd,
-                        (HMENU)ID_MINE_ICON, // ButtonID
-                        hInstance,
-                        NULL);
-
-            SendMessage(mineButton, BM_SETIMAGE, IMAGE_ICON, (LPARAM)hFlagIcon);
             break;
         }
 
         case WM_COMMAND:
-            if (HIWORD(wParam) == BN_CLICKED)
+            // Menu
+            if (lParam == 0)
+            {
+                switch (LOWORD(wParam))
+                {
+                    case ID_GAME_NEW:
+                        ResetGame(hwnd, gridSize, numMines);
+                        break;
+
+                    case ID_GAME_EXIT:
+                        PostMessage(hwnd, WM_CLOSE, 0, 0);
+                        break;
+
+                    case ID_HELP:
+                        MessageBox(hwnd, TEXT("Help in progress"), TEXT("Help"), MB_OK);
+                        break;
+
+                    case ID_GAME_BEGINNER:
+                        CheckMenuItem(hMenu, LOWORD(wParam), MF_CHECKED);
+                        CheckMenuItem(hMenu, ID_GAME_INTERMEDIATE, MF_UNCHECKED);
+                        CheckMenuItem(hMenu, ID_GAME_EXPERT, MF_UNCHECKED);
+                        ResetGame(hwnd, GRID_SIZE_BEGINNER, NUM_MINES_BEGINNER);
+                        break;
+
+                    case ID_GAME_INTERMEDIATE:
+                        CheckMenuItem(hMenu, LOWORD(wParam), MF_CHECKED);
+                        CheckMenuItem(hMenu, ID_GAME_BEGINNER, MF_UNCHECKED);
+                        CheckMenuItem(hMenu, ID_GAME_EXPERT, MF_UNCHECKED);
+                        ResetGame(hwnd, GRID_SIZE_INTERNEDIATE, NUM_MINES_INTERMEDIATE);
+                        break;
+
+                    case ID_GAME_EXPERT:
+                        CheckMenuItem(hMenu, LOWORD(wParam), MF_CHECKED);
+                        CheckMenuItem(hMenu, ID_GAME_BEGINNER, MF_UNCHECKED);
+                        CheckMenuItem(hMenu, ID_GAME_INTERMEDIATE, MF_UNCHECKED);
+                        ResetGame(hwnd, GRID_SIZE_EXPERT, NUM_MINES_EXPERT);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            // Buttons
+            else
             {
                 int buttonID = LOWORD(wParam);
-
-                if(buttonID == ID_MINE_ICON)
-                {
-                    cout<<"Mine button clicked"<<endl;
-                    break;
-                }
-
-                int x = buttonID / GRID_SIZE;
-                int y = buttonID % GRID_SIZE;
+                int x = buttonID / gridSize;
+                int y = buttonID % gridSize;
 
                 if (!isGridInitialized)
                 {
@@ -156,8 +182,58 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
                 {
                     RevealCells(x, y);
                 }
+
+                if (grid[x][y].isMine && !grid[x][y].isFlagged)
+                {
+                    PaintFailedMine(x, y, hwnd, hInstance);
+                    for (int xi = 0; xi < gridSize; ++xi)
+                    {
+                        for (int yi = 0; yi < gridSize; ++yi)
+                        {
+                            if ((grid[xi][yi].isMine && !grid[xi][yi].isRevealed) && !grid[xi][yi].isFlagged)
+                            {
+                                ShowWindow(gridButtons[xi][yi], SW_HIDE);
+                            }
+                        }
+                    }
+                    MessageBox(hwnd, TEXT("You Lost!"), TEXT("Minesweeper"), MB_OK);
+                    ResetGame(hwnd, gridSize, numMines);
+                    break;
+                }
+
+                if (revealedCells == gridSize - numMines)
+                {
+                    MessageBox(hwnd, TEXT("You Won!"), TEXT("Minesweeper"), MB_OK);
+                }
+
             }
             break;
+
+        case WM_CONTEXTMENU:
+        {
+            HWND hButton = (HWND)wParam;
+            for (int x = 0; x < gridSize; ++x)
+            {
+                for (int y = 0; y < gridSize; ++y)
+                {
+                    if (hButton == gridButtons[x][y])
+                    {
+                        if (!grid[x][y].isFlagged)
+                        {
+                            SendMessage(hButton, BM_SETIMAGE, IMAGE_ICON, (LPARAM)hFlagIcon);
+                            grid[x][y].isFlagged = true;
+                        }
+                        else
+                        {
+                            SendMessage(hButton, BM_SETIMAGE, IMAGE_ICON, (LPARAM)hButtonIcon);
+                            grid[x][y].isFlagged = false;
+                        }
+                        break;
+                    }
+                }
+            }
+            break;
+        }
 
         case WM_PAINT:
             PaintComponents(hwnd, hInstance);
@@ -186,13 +262,13 @@ void PaintGrid(HDC hdc)
     HPEN hPen = CreatePen(PS_SOLID, 1, GRAY);
     SelectObject(hdc, hPen);
 
-    for (int i = 0; i <= GRID_SIZE; ++i)
+    for (int i = 0; i <= gridSize; ++i)
     {
         // Vertical lines
         double verticalFromX = i * CELL_SIZE + BORDER_WIDTH;
         double verticalFromY = TOOLBAR_HEIGHT;
         double verticalToX = i * CELL_SIZE  + BORDER_WIDTH;
-        double verticalToY = GRID_SIZE * CELL_SIZE + TOOLBAR_HEIGHT;
+        double verticalToY = gridSize * CELL_SIZE + TOOLBAR_HEIGHT;
 
         MoveToEx(hdc, verticalFromX, verticalFromY, NULL);
         LineTo(hdc, verticalToX, verticalToY);
@@ -200,7 +276,7 @@ void PaintGrid(HDC hdc)
         // Horizontal lines
         double horizontalFromX = BORDER_WIDTH;
         double horizontalFromY = i * CELL_SIZE + TOOLBAR_HEIGHT;
-        double horizontalToX = GRID_SIZE * CELL_SIZE + BORDER_WIDTH;
+        double horizontalToX = gridSize * CELL_SIZE + BORDER_WIDTH;
         double horizontalToY = i * CELL_SIZE + TOOLBAR_HEIGHT;
 
         MoveToEx(hdc, horizontalFromX, horizontalFromY, NULL);
@@ -208,6 +284,23 @@ void PaintGrid(HDC hdc)
     }
 
     DeleteObject(hPen);
+}
+
+void PaintFailedMine(int x, int y, HWND hwnd, HINSTANCE hInstance)
+{
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+    RECT rect;
+
+    SetRect(&rect,
+        x * CELL_SIZE + BORDER_WIDTH,
+        y * CELL_SIZE + TOOLBAR_HEIGHT,
+        (x + 1) * CELL_SIZE + BORDER_WIDTH,
+        (y + 1) * CELL_SIZE + TOOLBAR_HEIGHT);
+
+    HICON hMineIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ID_MINE_FAILED_ICON));
+    DrawIcon(hdc, rect.left, rect.top, hMineIcon);
+
 }
 
 void PaintValues(HDC hdc, HINSTANCE hInstance)
@@ -218,9 +311,9 @@ void PaintValues(HDC hdc, HINSTANCE hInstance)
     HGDIOBJ oldFont = SelectObject(hdc, hfont);
     SetBkMode(hdc, TRANSPARENT);
 
-    for (int x = 0; x < GRID_SIZE; ++x)
+    for (int x = 0; x < gridSize; ++x)
     {
-        for (int y = 0; y < GRID_SIZE; ++y)
+        for (int y = 0; y < gridSize; ++y)
         {
             SetRect(&rect,
                 x * CELL_SIZE + BORDER_WIDTH,
@@ -290,10 +383,10 @@ void InitializeGrid(int startX, int startY)
 {
     srand((unsigned)time(NULL));
     int minesPlaced = 0;
-    while (minesPlaced < NUM_MINES)
+    while (minesPlaced < numMines)
     {
-        int x = rand() % GRID_SIZE;
-        int y = rand() % GRID_SIZE;
+        int x = rand() % gridSize;
+        int y = rand() % gridSize;
         if (!grid[x][y].isMine && !areNeighbors(startX, startY, x, y) && x != startX && y != startY)
         {
             grid[x][y].isMine = true;
@@ -301,9 +394,9 @@ void InitializeGrid(int startX, int startY)
         }
     }
 
-    for (int x = 0; x < GRID_SIZE; ++x)
+    for (int x = 0; x < gridSize; ++x)
     {
-        for (int y = 0; y < GRID_SIZE; ++y)
+        for (int y = 0; y < gridSize; ++y)
         {
             if (grid[x][y].isMine) continue;
             int mineCount = 0;
@@ -312,7 +405,7 @@ void InitializeGrid(int startX, int startY)
                 for (int dy = -1; dy <= 1; ++dy)
                 {
                     int nx = x + dx, ny = y + dy;
-                    if (nx >= 0 && ny >= 0 && nx < GRID_SIZE && ny < GRID_SIZE && grid[nx][ny].isMine)
+                    if (nx >= 0 && ny >= 0 && nx < gridSize && ny < gridSize && grid[nx][ny].isMine)
                     {
                         ++mineCount;
                     }
@@ -325,11 +418,12 @@ void InitializeGrid(int startX, int startY)
 
 void RevealCells(int x, int y)
 {
-    if (x < 0 || y < 0 || x >= GRID_SIZE || y >= GRID_SIZE) return;
+    if (x < 0 || y < 0 || x >= gridSize || y >= gridSize) return;
     if (grid[x][y].isRevealed || grid[x][y].isFlagged) return;
 
     grid[x][y].isRevealed = true;
     ShowWindow(gridButtons[x][y], SW_HIDE);
+    revealedCells++;
 
     if (grid[x][y].adjacentMines != 0 || grid[x][y].isMine) return;
 
@@ -351,11 +445,67 @@ bool areNeighbors(int x1, int y1, int x2, int y2)
     return (dx <= 1 && dy <= 1 && (dx + dy > 0));
 }
 
+void ResetGame(HWND hwnd, int newGridSize, int newNumMines)
+{
+    for (int x = 0; x < gridSize; ++x)
+    {
+        for (int y = 0; y < gridSize; ++y)
+        {
+            if (gridButtons[x][y]) DestroyWindow(gridButtons[x][y]);
+        }
+    }
+
+    gridSize = newGridSize;
+    numMines = newNumMines;
+    revealedCells = 0;
+
+    grid.clear();
+    gridButtons.clear();
+    grid.resize(newGridSize, vector<Cell>(newGridSize));
+    gridButtons.resize(newGridSize, vector<HWND>(newGridSize));
+
+    HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
+    HICON hButtonIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ID_BUTTON_ICON));
+
+    // Resize window
+    SetWindowPos(
+        hwnd, NULL, 0, 0,
+        gridSize * CELL_SIZE + 16 + BORDER_WIDTH * 2,
+        gridSize * CELL_SIZE + 36 + TOOLBAR_HEIGHT + BORDER_WIDTH + NAV_BAR_HEIGHT,
+        SWP_NOMOVE | SWP_NOZORDER
+    );
+
+    // Initialize grid buttons
+    for (int x = 0; x < newGridSize; ++x)
+    {
+        for (int y = 0; y < newGridSize; ++y)
+        {
+            gridButtons[x][y] = CreateWindow(
+                _T("BUTTON"),
+                _T(""),
+                WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON | BS_ICON,
+                x * CELL_SIZE + BORDER_WIDTH + 1,
+                y * CELL_SIZE + TOOLBAR_HEIGHT,
+                CELL_SIZE,
+                CELL_SIZE,
+                hwnd,
+                (HMENU)(x * newGridSize + y),
+                hInstance,
+                NULL);
+
+            SendMessage(gridButtons[x][y], BM_SETIMAGE, IMAGE_ICON, (LPARAM)hButtonIcon);
+        }
+    }
+
+    isGridInitialized = false;
+}
+
+
 void PrintGrid()
 {
-    for(int y = 0; y < GRID_SIZE; ++y)
+    for(int y = 0; y < gridSize; ++y)
     {
-        for(int x = 0; x < GRID_SIZE; ++x)
+        for(int x = 0; x < gridSize; ++x)
         {
             if (grid[x][y].isMine)
             {
